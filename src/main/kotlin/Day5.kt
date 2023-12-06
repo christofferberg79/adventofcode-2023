6a14c0414d290a3eb18b94a0e640ed8dc9747197
Christoffer Berg <christoffer.berg@collectum.se>
2023-12-06 11:07:44 +0100

    Explanation of day 5, part 2

--- src/main/kotlin/Day5.kt
+++ src/main/kotlin/Day5.kt
@@ -31,27 +31,52 @@ class Day5(private val input: List<String>) {
 
     fun part2(): Long {
         val lists = input.split("")
-        val seeds = lists.first().single().substringAfter(": ").split(" ").map(String::toLong)
+        val seedRanges = lists.first().single().substringAfter(": ").split(" ").map(String::toLong)
             .windowed(2, 2)
             .map { (start, length) -> start..<start + length }
 
-        val maps = lists.drop(1).map { mapInput ->
+        val conversionMaps = lists.drop(1).map { mapInput ->
             mapInput.drop(1).map { mapLine ->
                 mapLine.split(" ").map(String::toLong).let { (dest, source, length) -> MapLine(dest, source, length) }
             }.sortedBy { it.sourceRange.first }
         }
 
-        return maps.fold(seeds) { ranges, map ->
-            ranges.flatMap { range ->
-                var remainder = range
+        // Pass each seed range through all the maps.
+        // For each conversion map, split each incoming range according to the conversion ranges of the map.
+        // This results in a new set of sub-ranges where any sub-range that overlaps a conversion range is
+        // converted according to that conversion range.
+        // The sub-ranges that do not overlap any conversion range are kept with their original values.
+        // The new sub-ranges are then passed to the next conversion map.
+        return conversionMaps.fold(seedRanges) { ranges, conversionMap ->
+            ranges.flatMap { incomingRange ->
+                // At this point have an incoming range and a map of conversion ranges
+                // Incoming range:       |----------------------------------------------------|
+                // Conversion ranges:            |---------|       |---------|         |---------|
+                // Resulting sub-ranges: |------||---------||-----||---------||-------||------|
+                //                        as is   converted  as is  converted  as is    converted
+
+                // Going from left to right, we chop off parts af the incoming range are either
+                // completely outside or completely inside a conversion range. The chopped off
+                // pieces are put in 'newRanges' and the rest is put in 'remainder'.
+
+                // After going through two of the conversion ranges in the example above,
+                // the state will look like this:
+                // incomingRange:        |----------------------------------------------------|
+                // conversionMap:                |---------|       |---------|         |---------|
+                // newRanges:            |------||---------||-----||---------|
+                // remainder:                                                 |---------------|
+                var remainder = incomingRange
                 val newRanges = mutableListOf<LongRange>()
-                for (line in map) {
-                    newRanges += remainder.before(line.sourceRange)
-                    newRanges += remainder.overlap(line.sourceRange).offset(line.diff)
-                    remainder = remainder.after(line.sourceRange)
+                for (conversionRange in conversionMap) {
+                    newRanges += remainder.before(conversionRange.sourceRange)
+                    newRanges += remainder.overlap(conversionRange.sourceRange).offset(conversionRange.diff)
+                    remainder = remainder.after(conversionRange.sourceRange)
                 }
                 newRanges += remainder
-                newRanges.filterNot { it.isEmpty() }
+
+                // Pass the new ranges to the next conversion map (excluding any inverted ranges
+                // that may have been created byt the before/ovelap/after code above)
+                return@flatMap newRanges.filterNot { it.isEmpty() }
             }
         }.minOf { it.first }
     }
